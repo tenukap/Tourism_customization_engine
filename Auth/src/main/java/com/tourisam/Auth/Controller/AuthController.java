f952ceb39558684c77346896e274b7a5403c4eee
Tenuka <tenukaperera1025@gmail.com>
2026-04-20 22:53:27 +0530

    started on auth Controller

--- Auth/src/main/java/com/tourisam/Auth/Controller/AuthController.java
+++ Auth/src/main/java/com/tourisam/Auth/Controller/AuthController.java
@@ -1,4 +1,19 @@
 package com.tourisam.Auth.Controller;
 
+import com.tourisam.Auth.Model.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/Auth")
 public class AuthController {
+
+    @PostMapping("/register")
+    public ResponseEntity <?> register(){
+        User user = new User();
+
+    }
+
 }
