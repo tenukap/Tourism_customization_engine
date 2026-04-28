fee96eefcde64d7cd44e80a10342d206ab266068
Tharusha Dilshan <tdilshan1025@gmail.com>
2026-04-28 11:37:50 +0530

    Added package service layer

--- Auth/src/main/java/com/tourisam/project/service/TourPackageService.java
+++ Auth/src/main/java/com/tourisam/project/service/TourPackageService.java
@@ -1,4 +1,33 @@
 package com.tourisam.project.service;
 
+import com.tourisam.project.model.TourPackage;
+import com.tourisam.project.repository.TourPackageRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class TourPackageService {
+
+    private final TourPackageRepository repository;
+
+    public TourPackageService(TourPackageRepository repository) {
+        this.repository = repository;
+    }
+
+    public TourPackage addPackage(TourPackage tourPackage) {
+        return repository.save(tourPackage);
+    }
+
+    public List<TourPackage> getAllPackages() {
+        return repository.findAll();
+    }
+
+    public TourPackage getPackageById(Long id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    public void deletePackage(Long id) {
+        repository.deleteById(id);
+    }
 }
