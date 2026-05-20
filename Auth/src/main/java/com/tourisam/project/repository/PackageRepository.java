b812c8cf80c5e25445594f4387df87b14a68ce2e
Tharusha Dilshan <tdilshan1025@gmail.com>
2026-05-20 15:53:25 +0530

    updated repository

--- Auth/src/main/java/com/tourisam/project/repository/PackageRepository.java
+++ Auth/src/main/java/com/tourisam/project/repository/PackageRepository.java
@@ -3,7 +3,8 @@ package com.tourisam.project.repository;
 import com.travelus.packagemanagement.model.TourPackage;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
