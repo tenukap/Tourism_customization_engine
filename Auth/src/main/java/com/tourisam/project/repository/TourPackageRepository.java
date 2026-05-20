4e1543d1ec33e8bac7a8d99bf061359b5f259f4b
IT25100257 <IT25100257@my.sliit.lk>
2026-05-20 23:42:53 +0530

    Removing Tourisam Modules

--- Auth/src/main/java/com/tourisam/project/repository/TourPackageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tourisam.project.repository;
-
-import com.tourisam.project.model.TourPackage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TourPackageRepository extends JpaRepository<TourPackage, Long> {
-}
